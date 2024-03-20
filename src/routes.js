82fdd49acb002f9f8b34ae187ec8973dfb97ff2f
unknown <mgradle@iastate.edu>
2024-03-19 19:27:33 -0500

    Create global authentication

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,17 @@ import Dashboard from "./Components/Dashboard"
 import ViewRecords from "./Components/ViewRecords"
 import CreateAccount from "./Components/CreateAccount"
 import Settings from "./Components/Settings"
-import { createRoutesFromElements } from "react-router-dom"
 import ImportRecords from "./Components/ImportRecords/ImportRecords"
 import Dish from "./Components/CreateRecords/Dish"
 
 const routes = [
-  { path: "/", element: <LoginSignup /> },
-  { path: "/dash", element: <Dashboard /> },
-  { path: "/setup", element: <CreateAccount />},
-  { path: "/settings", element: <Settings />},
-  { path: "/view", element: <ViewRecords /> },
-  { path: "/import", element: <ImportRecords />},
-  { path: "/dish", element: <Dish/>},
-  { path: "/setup", element: <CreateAccount />}
+  { path: "/", element: <LoginSignup />, isProtected: false },
+  { path: "/dash", element: <Dashboard />, isProtected: true },
+  { path: "/setup", element: <CreateAccount />, isProtected: true },
+  { path: "/settings", element: <Settings />, isProtected: true },
+  { path: "/view", element: <ViewRecords />, isProtected: true },
+  { path: "/import", element: <ImportRecords />, isProtected: true },
+  { path: "/dish", element: <Dish />, isProtected: true },
 ]
 
 export default routes
