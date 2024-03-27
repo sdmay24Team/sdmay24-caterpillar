dfaac1fc7b905c4987322dfca671e897dcfae5b6
khawken <khawken@iastate.edu>
2024-03-27 06:14:02 -0500

    More caterpillar form work

--- src/Components/CreateRecords/Caterpillar.jsx
+++ src/Components/CreateRecords/Caterpillar.jsx
@@ -8,11 +8,14 @@ const Form = () => {
     date: '',
     site: '',
     location: '',
-    plant: ''
+    time: '',
+    plant: '',
+    stage: ''
   });
 
   const [currentField, setCurrentField] = useState('date');
   const [tempPlant, setTempPlant] = useState('');
+  const [eggColor, setEggColor] = useState('');
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [currentField]: e.target.value });
@@ -23,14 +26,14 @@ const Form = () => {
   };
 
   const handleNext = () => {
-    const fields = ['date', 'site', 'location', 'plant'];
+    const fields = ['date', 'site', 'location', 'time', 'plant', 'stage'];
     const currentIndex = fields.indexOf(currentField);
     const nextIndex = (currentIndex + 1) % fields.length;
     setCurrentField(fields[nextIndex]);
   };
 
   const handlePrevious = () => {
-    const fields = ['date', 'site', 'location', 'plant'];
+    const fields = ['date', 'site', 'location', 'time','plant', 'stage'];
     const currentIndex = fields.indexOf(currentField);
     const previousIndex = (currentIndex - 1 + fields.length) % fields.length;
     setCurrentField(fields[previousIndex]);
@@ -43,6 +46,10 @@ const Form = () => {
   const handlePlantUpdate = () => {
     setFormData({ ...formData, plant: tempPlant });
     setTempPlant('');
+  };
+
+  const handleEggColorChange = (e) => {
+    setEggColor(e.target.value);
   };
   
 
@@ -69,16 +76,33 @@ const Form = () => {
     { value: 'Other', label: 'Other'}
   ];
 
+  const stageOptions = [
+    { value: '', label: 'Select...'},
+    { value: 'egg', label: 'Egg' },
+    { value: '1st instar', label: '1st Instar' },
+    { value: '2nd instar', label: '2nd Instar' },
+    { value: '3rd instar', label: '3rd Instar' },
+    { value: '4th instar', label: '4th Instar' },
+    { value: '5th instar', label: '5th Instar' },
+    { value: 'pre-pupa', label: 'Pre-Pupa' },
+    { value: 'pupa', label: 'Pupa' }
+  ];
+
   return (
     <div className='container1'>
       <NavBar />
+      <div className="header">
+        <div className="text2">New Caterpillar</div>
+      </div>
       <div>
         <label className='text'>Select Field: </label>
         <select value={currentField} onChange={(e) => setCurrentField(e.target.value)}>
           <option value="date">Date Found</option>
           <option value="site">Site Found</option>
           <option value="location">Location(UTM)</option>
+          <option value="time">Time</option>
           <option value="plant">Host Plant</option>
+          <option value="stage">Stage</option>
         </select>
       </div>
   
@@ -98,10 +122,16 @@ const Form = () => {
           />
         </div>
       )}
-         {currentField === 'location' && (
+      {currentField === 'location' && (
         <div>
           <label className='text'>Location:</label>
-          <input type="text" value={formData.age} onChange={handleInputChange} />
+          <input type="text" value={formData.location} onChange={handleInputChange} />
+        </div>
+      )}
+       {currentField === 'time' && (
+        <div>
+          <label className='text'>Time:</label>
+          <input type="time" value={formData.time} onChange={handleInputChange} />
         </div>
       )}
       {currentField === 'plant' && (
@@ -120,6 +150,33 @@ const Form = () => {
           )}
         </div>
       )}
+      {currentField === 'stage' && (
+        <div>
+        <label className='text'>Stage:</label>
+        <select value={formData.stage} onChange={(e) => handleDropdownChange('stage', e.target.value)}>
+            {stageOptions.map(option => (
+              <option key={option.value} value={option.value}>{option.label}</option>
+            ))}
+          </select>
+  
+        {formData.stage === 'egg' && (
+          <div>
+            <label className='text'>Choose Egg Color:</label>
+            <select value={eggColor} onChange={handleEggColorChange}>
+              <option value="">Select...</option>
+              <option value="blue">Blue</option>
+              <option value="white">White</option>
+              <option value="yellow">Yellow</option>
+              <option value="orange">Orange</option>
+              <option value="orange/red">Orange/Red</option>
+              <option value="brown">Brown</option>
+            </select>
+          </div>
+        )}
+
+        
+      </div>
+      )}
 <div className='buttonholder1'>
       <button className='button1' onClick={handlePrevious}>Previous</button>
       <button className='button1' onClick={handleNext}>Next</button>
@@ -129,7 +186,10 @@ const Form = () => {
         <p>Date: {formData.date}</p>
         <p>Site: {formData.site}</p>
         <p>Location: {formData.location}</p>
+        <p>Time: {formData.time}</p>
         <p>Plant: {formData.plant}</p>
+        <p>Stage: {formData.stage}</p>
+        <p>Egg color: {eggColor}</p>
       </div>
     </div>
   );
